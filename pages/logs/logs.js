b4d7394cb47e598b5ecb6b59ea47d33a9c5f9273
CZCFighting <1247405060@qq.com>
2018-05-04 17:17:23 +0800

    url

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,20 +1,24 @@
 //logs.js
 const util = require('../../utils/util.js')
-
 Page({
   data: {
-    menuitems: [
-      { text: '我的关注', url: '/pages/userfollow/follow', icon: '/images/userInfo.png' },
-      { text: '帮助反馈', url: '', icon: '/images/help.png'},
-      { text: '联系客服', url: '', icon: '/images/connection.png' },
-      { text: '关于我们', url: '', icon: '/images/about.png' }
-    ]
+    menuitems: [],
+    userInfo : null
   },
   onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
+    wx.getStorage({
+      key: 'userInfo',
+      success: res=> {
+        this.setData({
+          userInfo: res.data,
+          menuitems: [
+            { text: '我的关注', url: '/pages/userfollow/follow?avatarUrl='+res.data.avatarUrl, icon: '/images/userInfo.png' },
+            { text: '帮助反馈', url: '', icon: '/images/help.png' },
+            { text: '联系客服', url: '', icon: '/images/connection.png' },
+            { text: '关于我们', url: '', icon: '/images/about.png' }
+          ],
+        })
+      },
     })
   }
 })
